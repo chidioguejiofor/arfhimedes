c868aed2638cd805f50fb9aa7a7fce96d1240706
chidioguejiofor <chidioguejiofor@gmail.com>
2020-03-05 11:09:24 +0100

    Added tests for authentication

---
+++ api/utils/messages.py
@@ -0,0 +1,5 @@
+SIGN_UP_SUCCESS = 'You have been successfully registered'
+USER_ALREADY_EXISTS = 'Either the email or the username already exists'
+LOGIN_FAILED = "Credentials not found"
+LOGIN_SUCCESS = 'Login was successful'
+MISSING_DATA = 'Missing data for required field.'
--- tests/test_sample.py
+++
@@ -1,8 +0,0 @@
-from api.models import User
-
-class TestSample:
-    def test_basic_setup_works(self):
-        assert 1 == 1
-
-    def test_can_make_db_calls(self, init_db):
-        assert User.query.count() == 0
